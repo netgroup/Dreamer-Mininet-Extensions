efca1961c9424f9e4e57f6193f3c6683fec5622f
Pier Luigi Ventre <pierventre@hotmail.com>
2014-07-10 14:02:41 +0200

    Added basic functionalities to MininetOSHI

--- mininet_extensions.py
+++ mininet_extensions.py
@@ -18,10 +18,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Implementation of Dreamer Nodes.
+# Implementation of Dreamer Extensions.
 #
 # @author Pier Luigi Ventre <pl.ventre@gmail.com>
 # @author Giuseppe Siracusano <a_siracusano@tin.it>
 # @author Stefano Salsano <stefano.salsano@uniroma2.it>
 #
 #/
+
+import sys
+
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from dreamer_nodes import OSHI, Router, LegacyL2Switch
+
+def MininetOSHI(Mininet):
+
+	def __init__(self, verbose=False):
+		Mininet.__init__(self, switch=LegacyL2Switch, controller=RemoteController, build=False)
+		self.ctrls = []
+		self.cr_oshis = []
+		self.pe_oshis = []
+		
+	def addOSHI(self, name, params):
+		if verbose:
+			print "addOSHI"
+		loopback = params.get("loopback", None)
+		if not loopback:
+			print "ERROR loopback not provided"
+			sys.exit(-2)
+		oshi = self.addHost(name, cls=OSHI, loopback)
+		return oshi
+
+	def addCrOSHI(self, name=None, params):
+		if verbose:
+			print "addCrOSHI"
+		if not name:
+			name = self.newCrName()
+		oshi = self.addOSHI(name, params)
+		self.cr_oshis.append(oshi)
+
+	def addPeOSHI(self, name=None, params):
+		if verbose:
+			print "addPeOSHI"
+		if not name:
+			name = self.newPeName()
+		oshi = self.addOSHI(name, params)
+		self.pe_oshis.append(oshi)
+
+	def addController(self, name=None, ip, tcp_port=6633)
+		if verbose:
+			print "addCTRL"
+		if not name:
+			name = self.newCtrlName()
+		ctrl = RemoteController(name, ip=ip, port=tcp_port)
+		self.ctrls.append(ctrl)
+		
+	def newCrName(self):
+		index = str(len(self.cr_oshis) + 1)
+		name = "cro%s" % index
+		return name	
+
+	def newPeName(self):
+		index = str(len(self.pe_oshis) + 1)
+		name = "peo%s" % index
+		return name	
+
+	def newCtrlName(self):
+		index = str(len(self.ctrls) + 1)
+		name = "ctr%s" % index
+		return name
+
+
